5892526c0f542bdc03dc81f65c77ccefb71d7a4c
Sarah Edkins <edkins.sarah@gmail.com>
2018-05-27 12:21:52 -0400

    babel tranform to create AST, babel-traverse to get parent function of param

--- test/components/DisplayName.js
+++ test/components/DisplayName.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-export const DisplayName = ({ name }) => (
-  <div className="flex flex-column flex-align-items-center pad-30 fancy-font">
-    {name}
-  </div>
-)
+export const DisplayName = ({ nameToDisplay }) => {
+  const color = "purple";
+  return (
+    <div className="flex flex-column flex-align-items-center pad-30 fancy-font">
+      {`${nameToDisplay} likes ${color}`}
+    </div>
+  );
+};
 
 export default DisplayName;
 
